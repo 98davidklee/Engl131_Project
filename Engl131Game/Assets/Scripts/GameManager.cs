eeb5211daac3a2c1500db2241bba34aab17a0099
David <98davidklee@gmail.com>
2018-04-21 16:57:01 -0700

    Added buttons and question number text

--- Engl131Game/Assets/Scripts/GameManager.cs
+++ Engl131Game/Assets/Scripts/GameManager.cs
@@ -10,12 +10,14 @@ public class GameManager : MonoBehaviour {
 	private int totalPoint; 
 	// the text file that has questions.
 	private StreamReader scanner;
-	// the question to be displayed on the game.
+	// the question to be displayed in the game.
 	public Text question;
 	// the answers to the questions.
 	private StreamReader answers;
 	// the question number that keeps track of the order of the questions.
 	private int number;
+	// The question number to be displayed in the game.
+	public Text questionNum;
 
 	/*
 	* Post: Constructs the game with the questions text file and default values.
@@ -23,6 +25,7 @@ public class GameManager : MonoBehaviour {
 	private void Start() {
 		number = 1;
 		totalPoint = 0;
+		questionNum.text = number + " / 20";
 		scanner = File.OpenText("questions.txt");
 		question.text = scanner.ReadLine();
 		answers = File.OpenText("answers.txt");
@@ -33,15 +36,17 @@ public class GameManager : MonoBehaviour {
 	* Post: Increments the total points if user guesses it correctly and prompts next question.
 	*/
 	public void TrueOrFalse(bool choice) {
-		if (number > 20) {
+		if (number == 20) {
+			questionNum.text = "";
 			question.text = "Your total score is: " + totalPoint;
 		} else {
 			if (Convert.ToBoolean(answers.ReadLine()) == choice) {
 				totalPoint += 5;
 			}
 			string nextLine = scanner.ReadLine();
-			question.text = nextLine;
 			number++;
+			questionNum.text = number + " / 20";
+			question.text = nextLine;
 		}
 	}
 }
