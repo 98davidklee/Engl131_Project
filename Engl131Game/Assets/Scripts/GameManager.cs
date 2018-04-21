e921869d2bd7f7dd541395234e07fb937267805e
David <98davidklee@gmail.com>
2018-04-21 16:04:17 -0700

    Commited with no change

---
+++ Engl131Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using System;
+
+public class GameManager : MonoBehaviour {
+	// the total point of an user.
+	private int totalPoint; 
+	// the text file that has questions.
+	private StreamReader scanner;
+	// the question to be displayed on the game.
+	public Text question;
+	// the answers to the questions.
+	private StreamReader answers;
+	// the question number that keeps track of the order of the questions.
+	private int number;
+
+	/*
+	* Post: Constructs the game with the questions text file and default values.
+	*/
+	private void Start() {
+		number = 1;
+		totalPoint = 0;
+		scanner = File.OpenText("questions.txt");
+		question.text = scanner.ReadLine();
+		answers = File.OpenText("answers.txt");
+	}
+
+	/*
+	* Pre: The game has at least one more question to ask, otherwise changes to next scene.
+	* Post: Increments the total points if user guesses it correctly and prompts next question.
+	*/
+	public void TrueOrFalse(bool choice) {
+		if (number > 20) {
+			question.text = "Your total score is: " + totalPoint;
+		} else {
+			if (Convert.ToBoolean(answers.ReadLine()) == choice) {
+				totalPoint += 5;
+			}
+			string nextLine = scanner.ReadLine();
+			question.text = nextLine;
+			number++;
+		}
+	}
+}
